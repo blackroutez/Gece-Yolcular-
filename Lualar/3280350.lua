6ffacf37e7b1b964f07023c3e017c50a99b06728
Mito-rium <metesoydan963@gmail.com>
2026-03-19 19:50:29 +0300

    Add/Update game: 3280350.lua via App Upload

---
+++ Lualar/3280350.lua
@@ -0,0 +1,17 @@
+-- Game: DEATH STRANDING 2: ON THE BEACH
+-- DLCs Included: 0
+-- --------------------------------------------------
+-- [Base Game]
+addappid(3280350, 1, "DEATH STRANDING 2: ON THE BEACH")
+-- [Depots]
+addappid(3280371, 0, "ac882a795ddeaa01d93fbaa70706d4ac44f853bce9e58f47e4a7f46a9e514724")
+addappid(3280431, 0, "01563e72a9a048efbed18b7f972969ce078fb8b3f296c4c7fd5806f4e5a7e4e6")
+addappid(3280461, 0, "31e60bd339d8422c9dbaad6469156ad3bcc7b6cd7992effff786218abc78ffa4")
+addappid(3280481, 0, "c22ebf7a136fadff92eed7242779256a9812b20016669abb2fc79308a9e4a3af")
+addappid(3280531, 0, "2fac2f0fffc0d1d24a60e8338be1ba6ce8b9f616f8d00bb0491d1fdbfb512cdc")
+addappid(3280561, 0, "F384C8412E26968F3592525B214076D4B085E536DF542DE84BC22C09FB44C851")
+addappid(3280600, 0, "393c5e207b5f65ede5c5cc4b83074e5e2ddf683b5b036e90fc3c84ad3e7250b2")
+--[[
+This file Generated using fares.top Website 
+-----> join discord Server : https://discord.gg/farestopp 
+]]
