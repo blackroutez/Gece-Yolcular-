a35e7d2b8285966c3fc630282e63411bdf480b06
Mito-rium <metesoydan963@gmail.com>
2026-05-13 17:46:10 +0300

    Add/Update game: 2483190.lua via App Upload

---
+++ Lualar/2483190.lua
@@ -0,0 +1,6 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2483190)
+addappid(2483191,0,"f2a09f0a31b4df681ec0d2d572f368dfa4cd6c37ae67ebdfd591cfbe1334fcd9")
