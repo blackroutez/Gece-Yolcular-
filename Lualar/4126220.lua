21b91e90ec04dc39bf9358a3a88de03ad5be37cb
Mito-rium <metesoydan963@gmail.com>
2026-05-19 23:05:45 +0300

    Add/Update game: 4126220.lua via App Upload

--- Lualar/4126220.lua
+++ Lualar/4126220.lua
@@ -1,8 +1,2 @@
--- Crusaders Que​​‌‌‌​​‌​​‌‌​‌‌‌​‌‌​​​‌‌​​‌‌‌​​​​‌‌​​‌​‌​​‌‌‌​​​​​‌‌​​​‌​​‌‌‌​​‌​‌‌​​‌​‌​‌‌​​​‌​​​‌‌​‌‌​​‌‌​​‌​​​​‌‌​‌​‌​​‌‌​​‌​​‌‌​​‌​​​‌‌​​‌‌​st : Hero Town
--- AppID 4126220 | Generated on 2026-05-19 20:03 UTC | openlua.cloud
-
--- Main Application
 addappid(4126220)
-
--- Content Depots (1)
-addappid(4126221, 1, "bbee22e7cd268244d481f200c8fd2ae401eaa3deb6b323bb5afca5eea1827635") -- Crusaders Quest : Hero Town - Windows
+addappid(4126221, 1, "bbee22e7cd268244d481f200c8fd2ae401eaa3deb6b323bb5afca5eea1827635")
