5777ac569bb34fb4cd93451c6cefe082e507a63b
Mito-rium <metesoydan963@gmail.com>
2026-05-23 17:39:12 +0300

    Add/Update game: 2215200.lua via App Upload

--- Lualar/2215200.lua
+++ Lualar/2215200.lua
@@ -1,20 +1,7 @@
--- 2215200's Lua and Manifest Created by Hubcap Manifest
--- LEGO® Batman™: Legacy of the Dark Knight
--- Created: May 20, 2026 at 00:32:59 EDT
--- Website: https://hubcapmanifest.com/
--- Total Depots: 3
--- Total DLCs: 1
--- Shared Depots: 2
-
--- MAIN APPLICATION
-addappid(2215200) -- LEGO® Batman™: Legacy of the Dark Knight
--- MAIN APP DEPOTS
-addappid(2215201, 1, "5d2bafc1381c29ebabb37bbab50e496548be24f6bf5d511e8d471b99d60cfbf3") -- Depot 2215201
-setManifestid(2215201, "6204730580506183716", 42126456373)
--- SHARED DEPOTS (from other apps)
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
-setManifestid(228989, "3514306556860204959", 39590283)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
-setManifestid(228990, "1829726630299308803", 102931551)
--- DLCS WITHOUT DEDICATED DEPOTS
-addappid(4168610) -- The Dark Knight Returns Suit
+addappid(2215200)
+addappid(4168610)
+addappid(4168620)
+addappid(4168630)
+addappid(4168640)
+addappid(4468750)
+addappid(2215201,0,"5d2bafc1381c29ebabb37bbab50e496548be24f6bf5d511e8d471b99d60cfbf3")
